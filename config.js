43794937641085ed6bfd2ced1180aa78732f11cd
moustafanassr99 <moustafanassr99@gmail.com>
2026-06-29 20:41:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   jsonbinId:  "6a410f90da38895dfe0b6035",
-  jsonbinKey: "$2a$10$GkaVoeERdWZKR6zbFgJvauDlU2QqRxih8vwP07eNclrVSGBIkMwmy",
+  jsonbinKey: "$2a$10$xoEdP4ya0qiSdeIFhuiKs.tR8zwdbVEh2dob0k.bfSPJfsf2.mjhK",
 
   forms: {
     opd:      "PASTE_OPD_FORM_LINK_HERE",
