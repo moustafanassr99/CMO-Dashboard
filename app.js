b0a0b22b1abe3cad7a13bc6d9cf8e5628e78732b
moustafanassr99 <moustafanassr99@gmail.com>
2026-07-23 13:00:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -119,6 +119,7 @@ async function loadDashboard() {
     renderKPIs();
     renderCapacity();
     renderProcedures();
+    renderOPDReferrals();
     renderTomorrow();
     renderVIP();
     renderCodes();
